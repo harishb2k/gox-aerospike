408b1fb9561773aa69c9781c130265208004759f
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-06-25 21:52:20 +0530

    initial code

---
+++ aerospike.go
@@ -0,0 +1,96 @@
+package dbaerospike
+
+import (
+    "fmt"
+    "github.com/aerospike/aerospike-client-go"
+    "github.com/harishb2k/gox-base/metrics"
+    "github.com/harishb2k/gox-errors"
+    "sync"
+)
+import "github.com/harishb2k/gox-db"
+import "github.com/harishb2k/gox-base"
+
+type Context struct {
+    Client   *aerospike.Client;
+    HostList string
+    Port     int
+    Keyspace string
+    onlyOnce sync.Once
+    *base.ApplicationContext
+}
+
+func New(context *Context) (idb db.IDb, err error) {
+    // Setup application context with defaults
+    if context.ApplicationContext == nil {
+        context.ApplicationContext = &base.ApplicationContext{}
+    }
+    context.SetupDefaultIfMissing()
+    return context, nil
+}
+
+func (context *Context) InitDatabase() (err error) {
+    context.onlyOnce.Do(func() {
+
+        client, err := aerospike.NewClient(context.HostList, context.Port)
+        if err != nil {
+        } else {
+            context.Client = client;
+        }
+
+        // Register all matrices
+        context.Metrics.RegisterCounter("aerospike_find_all", "aerospike_find_all help")
+        context.Metrics.RegisterCounter("aerospike_find_all_error", "aerospike_find_all_error help")
+
+    })
+    return
+}
+
+func (context *Context) FindOne(queryString string, mapper db.RowMapper, val ...interface{}) (result interface{}, e error) {
+    defer metrics.LogMetricFunc(context.Metrics, e, "aerospike_find_all", "aerospike_find_all_error")
+
+    // Ensure we have a session before we make any call
+    if e = context.ensureSession(); e != nil {
+        return
+    }
+
+    if key, err := aerospike.NewKey(context.Keyspace, base.Stringify(val[0]), base.Stringify(val[1])); err != nil {
+        e = &errors.ErrorObj{
+            Name:        db.DatabaseErrorUnknown,
+            Err:         err,
+            Description: fmt.Sprintf("Failed to create new key [%s]", base.Stringify(val[0])),
+        }
+    } else {
+        if record, err := context.Client.Get(nil, key); err == nil {
+            e = &errors.ErrorObj{
+                Name:        db.DatabaseErrorRecordNotFound,
+                Err:         err,
+                Description: fmt.Sprintf("Failed to read data for key [%s]", base.Stringify(val[0])),
+            }
+        } else {
+            result, e = mapper.Map(record)
+        }
+    }
+    return
+}
+
+func (context *Context) Persist(queryString string, val ...interface{}) (e error) {
+    return errors.New("Not implemented")
+}
+
+func (context *Context) FindAll(queryString string, mapper db.RowMapper, val ...interface{}) (result []interface{}, e error) {
+    return nil, errors.New("Not implemented")
+}
+
+func (context *Context) Execute(queryString string, val ...interface{}) (result interface{}, e error) {
+    return nil, errors.New("Not implemented")
+}
+
+func (context *Context) ensureSession() (err error) {
+    if context.Client == nil {
+        return &errors.ErrorObj{
+            Name: "db_session_is_null",
+            Err:  errors.New("session is not created, you need to initiate it by calling InitDatabase() once"),
+        }
+    }
+    return
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/harishb2k/gox-aerospike
+
+go 1.13
+
+require (
+	github.com/aerospike/aerospike-client-go v3.0.1+incompatible
+	github.com/harishb2k/gox-base v0.0.5
+	github.com/harishb2k/gox-db v0.0.4
+	github.com/harishb2k/gox-errors v0.0.4
+	github.com/onsi/ginkgo v1.13.0 // indirect
+	github.com/yuin/gopher-lua v0.0.0-20200603152657-dc2b0ca8b37e // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+)
